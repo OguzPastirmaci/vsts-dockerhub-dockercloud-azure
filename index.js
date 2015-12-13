2fa160fb8d416c5a810eaec27851f87ee8a3dfb9
Oguz Pastirmaci <oguzp@microsoft.com>
2015-12-13 14:35:24 -0800

    Update index.js & index.jade

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {  
-      res.render('index', {hostname: hostname});
+      res.render('index', { hostname: 'hostname'});
   });
 });
 
